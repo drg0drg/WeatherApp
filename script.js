74177206776195f805de6b31e82ca5f8792b38bf
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-21 23:00:01 +0100

    Tuesday

--- script.js
+++ script.js
@@ -12,21 +12,36 @@ inputEl = document.getElementById("input");
 searchBtn = document.getElementById("search-btn");
 citiesListEl = document.getElementById("cities-list");
 containerEl = document.getElementById("container");
+mainWeatherEl = document.getElementById("main-weather");
+mainWeatherTitle = document.getElementById("main-weather-title");
+mainWeatherTitleText = document.getElementById("main-weather-title-text");
+mainWeatherTitleDate = document.getElementById("main-weather-title-date");
+mainWeatherInfo = document.getElementById("main-weather-info");
+tempEl = document.getElementById("temp");
+humidityEl = document.getElementById("humidity");
+windEl = document.getElementById("wind");
+UVfactorEl = document.getElementById("UV-factor");
+
+forecastBlock = document.getElementById("forecast-block");
+nextDay1 = document.getElementById("next-day-1");
+nextDay2 = document.getElementById("next-day-2");
+nextDay3 = document.getElementById("next-day-3");
+nextDay4 = document.getElementById("next-day-4");
+nextDay5 = document.getElementById("next-day-5");
+// nextDay1Date = document.getElementById("next-day-1-date");
+
 
 
 
 var nowMoment = moment();
 
-// moment JS
-// currentHour = moment().format("kk");
-
 //Showing the date using Moment.js
 var nowDateShown = document.getElementById("navbar-date");
-nowDateShown.textContent = nowMoment.format("DD-MMM-YYYY")
+nowDateShown.textContent = nowMoment.format("DD-MMM-YYYY");
 
 //Showing the time using Moment.js
 var nowHourShown = document.getElementById("navbar-hour");
-nowHourShown.textContent = nowMoment.format("kk:mm")
+nowHourShown.textContent = nowMoment.format("kk:mm");
 
 
 
@@ -36,6 +51,8 @@ nowHourShown.textContent = nowMoment.format("kk:mm")
 var userInput;
 var localStorageKey = 0;
 var userCityList;
+var cityLatitude;
+var cityLongitude;
 
 
 
@@ -56,12 +73,12 @@ function createUserCity () {
 
         //creating the box
         userCityList = document.createElement("div");
-        userCityList.setAttribute("class", "prev-Search");
+        userCityList.setAttribute("class", "user-city-list");
         citiesListEl.appendChild(userCityList);
 
         //creating the text <p> inside the box
         var userCityListText = document.createElement("p");
-        userCityListText.setAttribute("class", "prev-Search-Text");
+        userCityListText.setAttribute("class", "user-city-list-Text");
         userCityList.appendChild(userCityListText);
         
         //taking the value from local storage and place it into the newly created div
@@ -78,7 +95,10 @@ function localStorageSave(){
         //saving user's input in localStorage
         userInput = input.value;
         localStorage.setItem(localStorageKey, userInput);
-        localStorageKey++;
+        if(localStorage != null){
+            localStorageKey++;
+        }
+        // localStorageKey++;
 }
 
 
@@ -89,6 +109,8 @@ searchBtn.addEventListener("click", function(e){
     localStorageSave();
     createUserCity ();
     getWeatherAPIdata();
+    getWeatherAPI5Days();
+    // getUVindex();
     // createMainAppInfo();
 })
 
@@ -126,15 +148,149 @@ function loadCitiesList(){
 function getWeatherAPIdata(){
     userInput = input.value;
     var city = userInput;
-    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=f729f6b644bc293d9f405e30b345dbd6";
+    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&uvi&units=metric&appid=f729f6b644bc293d9f405e30b345dbd6";
     console.log("user input city is  " + city)
     
     fetch(api)
     .then(res => res.json())
     .then(data => {
         console.log(data);
+
+        mainWeatherTitleText.textContent = data.name;
+        tempEl.textContent = "Temperature: " + data.main.temp + "°C";
+        humidityEl.textContent = "Humidity: " + data.main.humidity;
+        windEl.textContent = "Wind speed: " + data.wind.speed + " m/s";
+
+        cityLatitude = data.coord.lat;
+        cityLongitude = data.coord.lon;
+        UVfactorEl.textContent = "cityLatitude is: " + cityLatitude;
     })
+
+// -------------------UV INDEX------------
+
+
+    // // correct synthax for openWeather UVindex API
+    // // http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
+    // var api2 = "http://api.openweathermap.org/data/2.5/uvi?appid=f729f6b644bc293d9f405e30b345dbd6&lat="+cityLatitude+"&lon="+cityLongitude;
+
+    // fetch(api2)
+    //     .then(res => res.json())
+    //     .then(data2 => {
+    //     console.log(data2);
+    //     })
+
+} //end of getWeatherAPIdata()
+
+
+
+
+// ---------------5 day forecast------------
+//------------------------------------------
+// correct synthax for openWeather 5 day forecast API
+// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+// my key    f729f6b644bc293d9f405e30b345dbd6
+
+
+
+function getWeatherAPI5Days(){
+    userInput = input.value;
+    var city = userInput;
+    var api= "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f729f6b644bc293d9f405e30b345dbd6";
+
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+
+        //bring all <next day dates> from HTML
+        dateNextDay1 = document.getElementById("next-day-1-date");
+        dateNextDay2 = document.getElementById("next-day-2-date");
+        dateNextDay3 = document.getElementById("next-day-3-date");
+        dateNextDay4 = document.getElementById("next-day-4-date");
+        dateNextDay5 = document.getElementById("next-day-5-date");
+
+        //bring all <next day icons> from HTML
+        iconNextDay1 = document.getElementById("next-day-1-image-box");
+
+        for (i=1; i<=5; i++){
+            var str = data.list[i].dt_txt;
+            str = str.substring(0, str.length-8);
+            // console.log(str);
+            // nextDay1Date.textContent = str;
+        }
+
+
+
+
+        //-------------loop try-----------------
+        // const ceva = data.list
+        // const altceva = ceva.slice(1, 6);
+        // console.log(altceva)
+
+        // altceva.forEach(element => {
+        //     const p = document.createElement("p");
+        //     p.textContent = element[0].dt_txt;
+        //     var forecastDay = document.getElementById("next-day-1");
+        //     forecastDay.appendChild(p);
+        // })
+
+
+
+
+        // dt_txt: "2020-04-22 00:00:00"
+
+    })
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------UV INDEX------------
+
+
+// function getUVindex(){
+//     userInput = input.value;
+//     var city = userInput;
+//     console.log("user input city is  " + city)
+//     // http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
+//     var api = "http://api.openweathermap.org/data/2.5/uvi?appid=f729f6b644bc293d9f405e30b345dbd6&lat="+cityLatitude+"&lon="+cityLongitude;
+
+//     fetch(api)
+//         .then(res => res.json())
+//         .then(data => {
+//         console.log(data);
+// })
+// }//end of getUVindex()
+
+
+
+
+
+
+//----------------------------------------------------
+
+
+
+
+// function fetchWeatherData(){
+// }
+
+// fetchWeatherData();
 
 
 //Function that creates <RHS div>
@@ -164,3 +320,4 @@ function getWeatherAPIdata(){
 //             forecastBlock.setAttribute("class", "forecast-block");
 //             RHSMain.appendChild(forecastBlock);
 // }
+
