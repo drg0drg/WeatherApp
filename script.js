d6607bf7ba1038b9f7670d6e5618d350b0545a16
drg0drg <dragomir_costin_dragos@yahoo.com>
2020-04-21 17:24:26 +0100

    Adding OpenWeather API

--- script.js
+++ script.js
@@ -11,6 +11,8 @@
 inputEl = document.getElementById("input");
 searchBtn = document.getElementById("search-btn");
 citiesListEl = document.getElementById("cities-list");
+containerEl = document.getElementById("container");
+
 
 
 var nowMoment = moment();
@@ -33,7 +35,7 @@ nowHourShown.textContent = nowMoment.format("kk:mm")
 
 var userInput;
 var localStorageKey = 0;
-var prevSearch;
+var userCityList;
 
 
 
@@ -47,26 +49,26 @@ window.onload = function() {
 
 
 
-//function <createDiv> dynamically creates div to hold user input for searching places
-function createDiv () {
+//function <createUserCity> dynamically creates a div for each of user's searches
+// These searches will load when page loads because they retreive info from localStorage
 
-    // for (i=0; i<localStorage.length; i++) {
+function createUserCity () {
 
         //creating the box
-        var prevSearch = document.createElement("div");
-        prevSearch.setAttribute("class", "prev-Search");
-        citiesListEl.appendChild(prevSearch);
+        userCityList = document.createElement("div");
+        userCityList.setAttribute("class", "prev-Search");
+        citiesListEl.appendChild(userCityList);
 
         //creating the text <p> inside the box
-        var prevSearchText = document.createElement("p");
-        prevSearchText.setAttribute("class", "prev-Search-Text");
-        prevSearch.appendChild(prevSearchText);
+        var userCityListText = document.createElement("p");
+        userCityListText.setAttribute("class", "prev-Search-Text");
+        userCityList.appendChild(userCityListText);
         
         //taking the value from local storage and place it into the newly created div
         for (localStorageKey=0; localStorageKey<localStorage.length; localStorageKey++) {
-            prevSearchText.innerHTML = localStorage.getItem(localStorageKey);
+            userCityListText.innerHTML = localStorage.getItem(localStorageKey);
             console.log("localStorageKey is " + localStorageKey);
-            console.log("prevSearchText is " + prevSearchText);
+            console.log("userCityListText is " + userCityListText.textContent);
         }
 }
 
@@ -85,40 +87,80 @@ function localStorageSave(){
 searchBtn.addEventListener("click", function(e){
     e.preventDefault();
     localStorageSave();
-    createDiv ();
+    createUserCity ();
+    getWeatherAPIdata();
+    // createMainAppInfo();
 })
 
 
 
 //function to build cities list and populate each div with values from localStorage.
 //this will be called inside <onload> function.
-//It needs to be separate process from createDiv (which is linked to searchBtn)
+//It needs to be separate process from createUserCity (which is linked to searchBtn)
 function loadCitiesList(){
     for (i=0; i<localStorage.length; i++) {
 
         //creating the box
-        var prevSearch = document.createElement("div");
-        prevSearch.setAttribute("class", "prev-Search");
-        citiesListEl.appendChild(prevSearch);
+        var userCityList = document.createElement("div");
+        userCityList.setAttribute("class", "prev-Search");
+        citiesListEl.appendChild(userCityList);
         
         //creating the text <p> inside the box
-        var prevSearchText = document.createElement("p");
-        prevSearchText.setAttribute("class", "prev-Search-Text");
-        prevSearch.appendChild(prevSearchText);
+        var userCityListText = document.createElement("p");
+        userCityListText.setAttribute("class", "prev-Search-Text");
+        userCityList.appendChild(userCityListText);
                 
         //taking the value from local storage and place it into the newly created div
-        prevSearchText.innerHTML = localStorage.getItem(i);
+        userCityListText.innerHTML = localStorage.getItem(i);
         console.log("localStorageKey is " + i);
-        console.log("prevSearchText is " + i);        
-        
+        console.log("userCityListText is " + i);        
     }
 }
 
 
+// -----------------WATHER API----------------------
+//--------------------------------------------------
+
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+// my key    f729f6b644bc293d9f405e30b345dbd6
+function getWeatherAPIdata(){
+    userInput = input.value;
+    var city = userInput;
+    var api = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=f729f6b644bc293d9f405e30b345dbd6";
+    console.log("user input city is  " + city)
+    
+    fetch(api)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+}
 
 
+//Function that creates <RHS div>
+// function createMainAppInfo() {
+//             //creating the box for RHS
+//             RHSMain = document.createElement("div");
+//             RHSMain.setAttribute("class", "RHS-main");
+//             containerEl.appendChild(RHSMain);
 
+//             //creating the main div for current city weather
+//             mainWeather = document.createElement("div");
+//             mainWeather.setAttribute("class", "main-weather");
+//             RHSMain.appendChild(mainWeather);
 
+//             //creating the title sub-block for the main weather section
+//             mainWeatherTitle = document.createElement("div");
+//             mainWeatherTitle.setAttribute("class", "main-weather-title")
+//             mainWeather.appendChild(mainWeatherTitle);
 
+//             //creating the info sub-block for the main weather section
+//             mainWeatherInfo = document.createElement("div");
+//             mainWeatherInfo.setAttribute("class", "main-weather-info")
+//             mainWeather.appendChild(mainWeatherInfo);
 
-
+//             //creating the div for the 5 days prognosis
+//             forecastBlock = document.createElement("div");
+//             forecastBlock.setAttribute("class", "forecast-block");
+//             RHSMain.appendChild(forecastBlock);
+// }
